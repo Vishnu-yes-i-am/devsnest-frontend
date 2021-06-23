1d52cb36f3984466b8439011180c61899732ec6b
Vishnn Mali <76401790+Vishnu-yes-i-am@users.noreply.github.com>
2021-06-23 22:30:54 +0530

    Delete day-7_qno2.js

--- day-7_qno2.js
+++
@@ -1,9 +0,0 @@
-var student ={
-    name:"David",
-    class:"6",
-    roll_no:"31",
-}
-console.log(student);
-delete student.class;
-console.log("After deleting class:-->");
-console.log(student);
