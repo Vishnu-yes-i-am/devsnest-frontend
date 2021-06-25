489e973c7290396a8ab52b840f3acb8bbe913699
Vishnn Mali <76401790+Vishnu-yes-i-am@users.noreply.github.com>
2021-06-25 16:49:28 +0530

    Add files via upload

---
+++ day10/day10.js
@@ -0,0 +1,52 @@
+var deck=['/cards/AH.jpg','/cards/5D.jpg','/cards/10C.jpg','/cards/7H.jpg','/cards/JD.jpg','/cards/QH.jpg','/cards/AH.jpg','/cards/5D.jpg','/cards/10C.jpg','/cards/7H.jpg','/cards/JD.jpg','/cards/QH.jpg']
+var flippedcards=[];
+var flipped=false;
+var hidden=0;
+var active=true;
+var backcards=document.querySelectorAll('.container');
+backcards.forEach(card => {
+    var temp=Math.floor((Math.random() * deck.length));
+    card.querySelector(".card").setAttribute("style","background:url("+deck[temp]+"); background-size: 160px 230px;");
+    card.setAttribute("id",deck[temp]);
+    deck.splice(temp,1);
+});
+backcards.forEach(card => {card.addEventListener('click',() =>{
+    if(active==true){
+    card.querySelector(".back").classList.toggle('flip');
+    card.querySelector(".card").classList.toggle('flip');
+    if(flipped==true){
+        active=false;
+        setTimeout(function(){
+        flipped=false;
+        var a=flippedcards[0];
+        if(a.id ===card.id){
+            a.setAttribute("style","visibility:hidden;");;
+            card.setAttribute("style","visibility:hidden;");
+            hidden+=2;
+            flippedcards.splice(0,1);
+        }
+        else{
+            if(flippedcards.length>0){
+            a.querySelector(".card.flip").classList.toggle('flip');
+            a.querySelector(".back.flip").classList.toggle('flip');
+        }
+            card.querySelector(".card.flip").classList.toggle('flip');
+            card.querySelector(".back.flip").classList.toggle('flip');
+            flippedcards.splice(0,1);
+        }
+        if(hidden===12){
+            alert("You Won.Thankyou for your time.");
+        }
+        active=true;
+
+    },2000);
+    }
+    
+    else{
+        flipped=true;
+        flippedcards.push(card);
+    }
+}
+});
+});
+
