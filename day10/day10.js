b02e98664ce88487693c9e1a64aadedfc0705613
Vishnn Mali <76401790+Vishnu-yes-i-am@users.noreply.github.com>
2021-06-26 01:46:43 +0530

    Add files via upload

--- day10/day10.js
+++ day10/day10.js
@@ -1,5 +1,7 @@
 var deck=['/cards/AH.jpg','/cards/5D.jpg','/cards/10C.jpg','/cards/7H.jpg','/cards/JD.jpg','/cards/QH.jpg','/cards/AH.jpg','/cards/5D.jpg','/cards/10C.jpg','/cards/7H.jpg','/cards/JD.jpg','/cards/QH.jpg']
 var flippedcards=[];
+var chances=5
+var hearts="";
 var flipped=false;
 var hidden=0;
 var active=true;
@@ -11,25 +13,41 @@ backcards.forEach(card => {
     deck.splice(temp,1);
 });
 backcards.forEach(card => {card.addEventListener('click',() =>{
+    if(flippedcards.length>0){
+        var a=flippedcards[0];
+    }
     if(active==true){
+        if(a!=card){
     card.querySelector(".back").classList.toggle('flip');
     card.querySelector(".card").classList.toggle('flip');
     if(flipped==true){
         active=false;
         setTimeout(function(){
         flipped=false;
-        var a=flippedcards[0];
         if(a.id ===card.id){
-            a.setAttribute("style","visibility:hidden;");;
+            a.setAttribute("style","visibility:hidden;");
             card.setAttribute("style","visibility:hidden;");
+            card.classList.add("dummy");
             hidden+=2;
             flippedcards.splice(0,1);
         }
         else{
             if(flippedcards.length>0){
+            a.classList.remove("dummy");
             a.querySelector(".card.flip").classList.toggle('flip');
             a.querySelector(".back.flip").classList.toggle('flip');
         }
+            chances-=1;
+            hearts="";
+            if(chances===0){
+                alert("Stupid ,you lose the GAME");
+                setTimeout(function(){window.location.reload();},250);
+            }
+            for (let i = 0; i < chances; i++) {
+                hearts+="&#129505;";
+            }
+            document.querySelector('.chances').innerHTML=hearts;
+            card.classList.remove("dummy");
             card.querySelector(".card.flip").classList.toggle('flip');
             card.querySelector(".back.flip").classList.toggle('flip');
             flippedcards.splice(0,1);
@@ -39,14 +57,15 @@ backcards.forEach(card => {card.addEventListener('click',() =>{
         }
         active=true;
 
-    },2000);
+    },800);
     }
     
     else{
         flipped=true;
         flippedcards.push(card);
+        card.classList.add("dummy");
     }
-}
+}}
 });
 });
 
